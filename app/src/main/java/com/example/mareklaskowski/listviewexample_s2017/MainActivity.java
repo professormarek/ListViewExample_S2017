fe66eae9274fd7b37f4b60c0366b834ac0a20fc3
marek.laskowski <marek.laskowski@senecacollege.ca>
2017-05-26 12:24:37 -0400

    left display the toast as a TODO for student exercise

--- app/src/main/java/com/example/mareklaskowski/listviewexample_s2017/MainActivity.java
+++ app/src/main/java/com/example/mareklaskowski/listviewexample_s2017/MainActivity.java
@@ -2,10 +2,30 @@ package com.example.mareklaskowski.listviewexample_s2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+    create an OnItemClickListener object and override its onItemClick method
+    (very much like the anonymous class example we did with Buttons)
+     */
+    private AdapterView.OnItemClickListener myListItemClickHandler = new AdapterView.OnItemClickListener(){
+
+        /*
+        parent is the ListView, position stores the index of the item that was clicked
+         */
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //get data from the view that was clicked...
+            String data = parent.getItemAtPosition(position).toString();
+            //for now just show a Toast.. but really we could do anything we want with the data...
+            //TODO: as an exercise
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
